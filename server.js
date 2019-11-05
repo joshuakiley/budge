b32652de8f2205d9f2f5e186faff5c799492b53c
Joshua Kiley <joshua.kiley.dev@gmail.com>
2019-11-05 14:33:32 -0500

    Create get logic in server.js

--- server.js
+++ server.js
@@ -24,3 +24,38 @@ mongoose.connection.once("open", () => {
 //==============================
 //    CONTROLLER SETUP
 //==============================
+const usersController = require("./controllers/users.js");
+const sessionsController = require("./controllers/sessions.js");
+
+//==============================
+//    MIDDLEWARE
+//==============================
+app.use(express.static(__dirname, "/public"));
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({
+    extended: false
+}));
+app.use(session({
+    secret: "DSF3o23eldSDdkas2120sdk",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use("/users", usersController);
+app.use("/sessions", sessionsController);
+
+//==============================
+//    GET INDEX
+//==============================
+app.get("/", (req, res) => {
+    res.render("index.ejs", {
+        currentUser: req.sessions.currentUser
+    });
+});
+
+app.get("/home", (req, res) => {
+    if (req.session.currentUser) {
+        res.render("/sessions/home.ejs");
+    } else {
+        res.redirect("/index.ejs");
+    }
+});
